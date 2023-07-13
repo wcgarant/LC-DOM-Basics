c8d1e129284fe6f1b7697afada6bd592841943cb
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-07-12 20:44:48 -0400

    declared button variables and created listeners to change border of colored buttons

--- scripts.js
+++ scripts.js
@@ -16,17 +16,32 @@ function init() {
     /*** STEP 1: CHANGE THE BORDER COLOR ***/
 
     // TODO: Declare objects to gain access to element properties.
-    
+    let redButton = document.getElementById("red");
+    let yellowButton = document.getElementById("yellow");
+    let blueButton = document.getElementById("blue");
 
     // TODO: Create listeners to change color of border to same hex values as buttons.
     // Note: DOM can't access style sheets, so just hard-code the hex codes here for now.
+    document.addEventListener("click", function(event){
+        redButton.style.border = "red"
+    });
+
+    document.addEventListener("click", function(event){
+        yellowButton.style.borderColor = "yellow"
+    });
+
+    document.addEventListener("mouseover", function(event){
+        blueButton.style.border = "blue"
+    });
 
 
 
     /*** STEP 2: CHANGE THE BORDER THICKNESS ***/
 
     // TODO: Declare objects to gain access to element properties.
-
+    let fourPix = document.getElementById("4");
+    let eightPix = document.getElementById("8");
+    let twelvePix = document.getElementById("12");
 
     // TODO: Create listeners to change thickness of box border.
 
